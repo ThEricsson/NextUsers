697e68dc0a82593ceec2ba2cfc78dd0d1aa1911a
Eric <56220682+ThEricsson@users.noreply.github.com>
2022-08-19 21:46:50 +0200

    Layout changes

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+
+import Layout from "../components/layout";
+
+export default function Home() {
+  return (
+    <>
+      <Layout title="Home">
+        <p>hola</p>
+      </Layout>
+    </>
+  );
+}
---
+++ pages/welcome.jsx
@@ -0,0 +1,18 @@
+export default function Welcome() {
+  return (
+    <main className="container">
+      <div className="text-center m-5 p-5">
+        <h1>
+          Welcome to <a className="text-primary">NextUsers</a>
+        </h1>
+        <p>
+          NextUsers! Una herramienta web enfocada en la gestión de usuarios y
+          roles.
+        </p>
+        <a className="btn btn-primary" href="/">
+          Vamos!
+        </a>
+      </div>
+    </main>
+  );
+}
